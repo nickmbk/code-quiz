9a6a177e874410095748394c3f54a82f4617c4c9
Nick <72912456+nickmbk@users.noreply.github.com>
2022-12-02 21:37:07 +0000

    add dummy questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,21 @@
+// questions
+var questions = [
+    {
+    question: "What is a div?",
+    answers: [
+        "a box",
+        "a cat",
+        "a kitchen utensil",
+        "a vacuum cleaner"
+    ]
+    },
+    {
+    question: "What is a ul?",
+    answers: [
+        "an unordered list",
+        "a cat",
+        "a kitchen utensil",
+        "a vacuum cleaner"
+        ]
+    }
+]
